ce0edfed11a84ebd1a92636de46e76d2a72a5192
AyimanyMooncat <mooncatayimany@proton.me>
2024-06-07 09:51:05 -0600

    [FIX] Test file

--- testing/main.cc
+++ testing/main.cc
@@ -1,28 +1,23 @@
 #include "acceleration.hh"
-#include "force.hh"
-#include "length.hh"
-#include "mass.hh"
-#include "time.hh"
-#include "unit_container.hh"
-#include "velocity.hh"
 
 #include <iostream>
 #include <ostream>
 
-constexpr lmc::units::acceleration::meters_per_second_squared g { 9.80665 };
-constexpr lmc::units::acceleration::feet_per_second_squared   f { g };
-
-long double constexpr x = f.get_measurement();
+constexpr lmc::units::acceleration::meters_per_second_squared gravity {
+    9.80665L
+};
+constexpr lmc::units::acceleration::feet_per_second_squared gravity_fts {
+    gravity
+};
 
 auto
 main() -> int
 {
     std::println(
         std::cout,
-        "{}kp = {}N = {}p",
-        kp.get_measurement(),
-        n.get_measurement(),
-        p.get_measurement()
+        "g = {}m/s = {}ft/s",
+        gravity.get_measurement(),
+        gravity_fts.get_measurement()
     );
 }
 
