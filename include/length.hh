2809ce7c91e186a91757534bd7b842557d608e39
Ayimany <mooncatayimany@proton.me>
2024-07-17 17:49:09 -0600

    [MOD] Switch length units to new format

--- include/length.hh
+++ include/length.hh
@@ -1,32 +1,70 @@
 #pragma once
 
-#include <wither/units/dimension.hh>
-#include <wither/units/kind.hh>
-#include <wither/units/magnitude.hh>
+#include <wither/units/units.hh>
 
 namespace units
 {
 
-using length_dimension = implementation::kind_dimension<
-    std::ratio<1>,
-    std::ratio<0>,
-    std::ratio<0>,
-    std::ratio<0>,
-    std::ratio<0>,
-    std::ratio<0>,
-    std::ratio<0>>;
+using length_dimension = impl::dimension<
+    std::ratio<1>, // length
+    std::ratio<0>, // mass
+    std::ratio<0>, // time
+    std::ratio<0>, // current
+    std::ratio<0>, // temperature
+    std::ratio<0>, // substance
+    std::ratio<0>  // luminous intensity
+    >;
 
-struct meters_kind: implementation::kind<length_dimension>
+namespace kind
 {
-    static constexpr double ratio = 1.0;
-};
+using metres = impl::kind<
+    length_dimension,
+    impl::prefix::none,
+    impl::ratio::base,
+    impl::delta::none>;
+using meters = metres;
 
-struct feet_kind: implementation::kind<length_dimension>
-{
-    static constexpr double ratio = 0.3048;
-};
+using feet           = impl::derived_kind<meters, std::ratio<381, 1250>>::value;
+using twips          = impl::derived_kind<feet, std::ratio<1, 17280>>::value;
+using thous          = impl::derived_kind<feet, std::ratio<1, 12000>>::value;
+using barleycorns    = impl::derived_kind<feet, std::ratio<1, 36>>::value;
+using inches         = impl::derived_kind<barleycorns, std::ratio<3>>::value;
+using hands          = impl::derived_kind<inches, std::ratio<4>>::value;
+using yards          = impl::derived_kind<feet, std::ratio<3>>::value;
+using chains         = impl::derived_kind<yards, std::ratio<22>>::value;
+using furlongs       = impl::derived_kind<chains, std::ratio<10>>::value;
+using miles          = impl::derived_kind<furlongs, std::ratio<8>>::value;
+using leagues        = impl::derived_kind<miles, std::ratio<3>>::value;
+using fathoms        = impl::derived_kind<meters, std::ratio<463, 250>>::value;
+using cables         = impl::derived_kind<fathoms, std::ratio<100>>::value;
+using nautical_miles = impl::derived_kind<cables, std::ratio<10>>::value;
+using links          = impl::derived_kind<feet, std::ratio<66, 100>>::value;
+using rods           = impl::derived_kind<links, std::ratio<25>>::value;
 
-using meters = implementation::magnitude<meters_kind>;
-using feet   = implementation::magnitude<feet_kind>;
+} // namespace kind
+
+/// The meter is the base unit of length in the International System of Units.
+/// It is defined as the distance travelled by light in `1 / 299792458` seconds
+/// (in a vaccuum).
+using metres = impl::magnitude<kind::metres, double>;
+using meters = metres;
+
+using feet           = impl::magnitude<kind::feet, double>;
+using twips          = impl::magnitude<kind::twips, double>;
+using thous          = impl::magnitude<kind::thous, double>;
+using barleycorns    = impl::magnitude<kind::barleycorns, double>;
+using inches         = impl::magnitude<kind::inches, double>;
+using hands          = impl::magnitude<kind::hands, double>;
+using yards          = impl::magnitude<kind::yards, double>;
+using chains         = impl::magnitude<kind::chains, double>;
+using furlongs       = impl::magnitude<kind::furlongs, double>;
+using miles          = impl::magnitude<kind::miles, double>;
+using leagues        = impl::magnitude<kind::leagues, double>;
+using fathoms        = impl::magnitude<kind::fathoms, double>;
+using cables         = impl::magnitude<kind::cables, double>;
+using nautical_miles = impl::magnitude<kind::nautical_miles, double>;
+using links          = impl::magnitude<kind::links, double>;
+using rods           = impl::magnitude<kind::rods, double>;
 
 } // namespace units
+
