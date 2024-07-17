e51b693f94c62e2cf8877b5a4e4efebbe1a6b288
Ayimany <mooncatayimany@proton.me>
2024-07-17 16:41:39 -0600

    [ADD] Magnitudes and swapping

--- include/units.hh
+++ include/units.hh
@@ -25,12 +25,13 @@ template <typename r> bool constexpr is_ratio_v = is_ratio<r>::value;
 template <typename r>
 concept ratio_cpt = is_ratio_v<r>;
 
-template <util::ratio_cpt ratio_t, typename fp>
+template <util::ratio_cpt ratio_t, typename internal_data_type>
 [[nodiscard]]
 static constexpr auto
-ratio_to_real() -> fp
+ratio_to_real() -> internal_data_type
 {
-    return static_cast<fp>(ratio_t::num) / static_cast<fp>(ratio_t::den);
+    return static_cast<internal_data_type>(ratio_t::num)
+         / static_cast<internal_data_type>(ratio_t::den);
 }
 
 } // namespace util
@@ -70,61 +71,67 @@ struct dimension: dimension_tag
     using temperature        = _temperature;
     using substance          = _substance;
     using luminous_intensity = _luminous_intensity;
-
-    template <typename other>
-    static constexpr bool equals
-        = std::ratio_equal_v<length, typename other::length>
-       && std::ratio_equal_v<mass, typename other::mass>
-       && std::ratio_equal_v<time, typename other::time>
-       && std::ratio_equal_v<current, typename other::current>
-       && std::ratio_equal_v<temperature, typename other::temperature>
-       && std::ratio_equal_v<substance, typename other::substance>
-       && std::ratio_equal_v<
-              luminous_intensity,
-              typename other::luminous_intensity>;
-
-    template <typename other>
-    using add = dimension<
-        std::ratio_add<length, typename other::length>,
-        std::ratio_add<mass, typename other::mass>,
-        std::ratio_add<time, typename other::time>,
-        std::ratio_add<current, typename other::current>,
-        std::ratio_add<temperature, typename other::temperature>,
-        std::ratio_add<substance, typename other::substance>,
-        std::ratio_add<luminous_intensity, typename other::luminous_intensity>>;
-
-    template <typename other>
-    using subtract = dimension<
-        std::ratio_subtract<length, typename other::length>,
-        std::ratio_subtract<mass, typename other::mass>,
-        std::ratio_subtract<time, typename other::time>,
-        std::ratio_subtract<current, typename other::current>,
-        std::ratio_subtract<temperature, typename other::temperature>,
-        std::ratio_subtract<substance, typename other::substance>,
-        std::ratio_subtract<
-            luminous_intensity,
-            typename other::luminous_intensity>>;
-
-    template <typename factor>
-    using times = dimension<
-        std::ratio_multiply<length, factor>,
-        std::ratio_multiply<mass, factor>,
-        std::ratio_multiply<time, factor>,
-        std::ratio_multiply<current, factor>,
-        std::ratio_multiply<temperature, factor>,
-        std::ratio_multiply<substance, factor>,
-        std::ratio_multiply<luminous_intensity, factor>>;
-
-    template <typename factor>
-    using divide = dimension<
-        std::ratio_divide<length, factor>,
-        std::ratio_divide<mass, factor>,
-        std::ratio_divide<time, factor>,
-        std::ratio_divide<current, factor>,
-        std::ratio_divide<temperature, factor>,
-        std::ratio_divide<substance, factor>,
-        std::ratio_divide<luminous_intensity, factor>>;
 };
+
+template <dimension_cpt dim_a, dimension_cpt dim_b>
+static constexpr bool equals
+    = std::ratio_equal_v<typename dim_a::length, typename dim_b::length>
+   && std::ratio_equal_v<typename dim_a::mass, typename dim_b::mass>
+   && std::ratio_equal_v<typename dim_a::time, typename dim_b::time>
+   && std::ratio_equal_v<typename dim_a::current, typename dim_b::current>
+   && std::ratio_equal_v<
+          typename dim_a::temperature,
+          typename dim_b::temperature>
+   && std::ratio_equal_v<typename dim_a::substance, typename dim_b::substance>
+   && std::ratio_equal_v<
+          typename dim_a::luminous_intensity,
+          typename dim_b::luminous_intensity>;
+
+template <dimension_cpt dim_a, dimension_cpt dim_b>
+using add = dimension<
+    std::ratio_add<typename dim_a::length, typename dim_b::length>,
+    std::ratio_add<typename dim_a::mass, typename dim_b::mass>,
+    std::ratio_add<typename dim_a::time, typename dim_b::time>,
+    std::ratio_add<typename dim_a::current, typename dim_b::current>,
+    std::ratio_add<typename dim_a::temperature, typename dim_b::temperature>,
+    std::ratio_add<typename dim_a::substance, typename dim_b::substance>,
+    std::ratio_add<
+        typename dim_a::luminous_intensity,
+        typename dim_b::luminous_intensity>>;
+
+template <dimension_cpt dim_a, dimension_cpt dim_b>
+using subtract = dimension<
+    std::ratio_subtract<typename dim_a::length, typename dim_b::length>,
+    std::ratio_subtract<typename dim_a::mass, typename dim_b::mass>,
+    std::ratio_subtract<typename dim_a::time, typename dim_b::time>,
+    std::ratio_subtract<typename dim_a::current, typename dim_b::current>,
+    std::ratio_subtract<
+        typename dim_a::temperature,
+        typename dim_b::temperature>,
+    std::ratio_subtract<typename dim_a::substance, typename dim_b::substance>,
+    std::ratio_subtract<
+        typename dim_a::luminous_intensity,
+        typename dim_b::luminous_intensity>>;
+
+template <dimension_cpt dim_a, util::ratio_cpt factor>
+using times = dimension<
+    std::ratio_multiply<typename dim_a::length, factor>,
+    std::ratio_multiply<typename dim_a::mass, factor>,
+    std::ratio_multiply<typename dim_a::time, factor>,
+    std::ratio_multiply<typename dim_a::current, factor>,
+    std::ratio_multiply<typename dim_a::temperature, factor>,
+    std::ratio_multiply<typename dim_a::substance, factor>,
+    std::ratio_multiply<typename dim_a::luminous_intensity, factor>>;
+
+template <dimension_cpt dim, util::ratio_cpt factor>
+using divide = dimension<
+    std::ratio_divide<typename dim::length, factor>,
+    std::ratio_divide<typename dim::mass, factor>,
+    std::ratio_divide<typename dim::time, factor>,
+    std::ratio_divide<typename dim::current, factor>,
+    std::ratio_divide<typename dim::temperature, factor>,
+    std::ratio_divide<typename dim::substance, factor>,
+    std::ratio_divide<typename dim::luminous_intensity, factor>>;
 
 using none = dimension<
     std::ratio<0>,
@@ -252,7 +259,10 @@ struct kind: kind_tag
     using delta     = delta_t;
 };
 
-template <kind_cpt kind_a, kind_cpt kind_b> struct conversion
+template <kind_cpt kind_a, kind_cpt kind_b>
+requires dimension::
+    equals<typename kind_a::dimension, typename kind_b::dimension>
+    struct conversion
 {
     using prefix_cvr
         = prefix::convert<typename kind_a::prefix, typename kind_b::prefix>;
@@ -263,20 +273,116 @@ template <kind_cpt kind_a, kind_cpt kind_b> struct conversion
     using delta_cvr
         = delta::convert<typename kind_a::delta, typename kind_b::delta>;
 
-    template <typename fp>
+    template <typename internal_data_type>
     [[nodiscard]]
     static constexpr auto
-    apply_to(fp measurement) -> fp
+    apply_to(internal_data_type measurement) -> internal_data_type
     {
-        constexpr fp prefix_v = util::ratio_to_real<prefix_cvr, fp>();
-        constexpr fp ratio_v  = util::ratio_to_real<ratio_cvr, fp>();
-        constexpr fp delta_v  = util::ratio_to_real<delta_cvr, fp>();
+        constexpr internal_data_type prefix_v
+            = util::ratio_to_real<prefix_cvr, internal_data_type>();
+        constexpr internal_data_type ratio_v
+            = util::ratio_to_real<ratio_cvr, internal_data_type>();
+        constexpr internal_data_type delta_v
+            = util::ratio_to_real<delta_cvr, internal_data_type>();
 
         return ratio_v * ((measurement * prefix_v) + delta_v);
     }
 };
 
+template <kind_cpt kind_t>
+using clone = kind<
+    typename kind_t::dimension,
+    typename kind_t::prefix_t,
+    typename kind_t::ratio,
+    typename kind_t::delta>;
+
+template <kind_cpt kind_t, dimension::dimension_cpt dimension_t>
+using swap_dimension = kind<
+    dimension_t,
+    typename kind_t::prefix_t,
+    typename kind_t::ratio,
+    typename kind_t::delta>;
+
+template <kind_cpt kind_t, util::ratio_cpt prefix_t>
+using swap_prefix = kind<
+    typename kind_t::dimension,
+    prefix_t,
+    typename kind_t::ratio,
+    typename kind_t::delta>;
+
+template <kind_cpt kind_t, util::ratio_cpt ratio_t>
+using swap_ratio = kind<
+    typename kind_t::dimension,
+    typename kind_t::prefix_t,
+    ratio_t,
+    typename kind_t::delta>;
+
+template <kind_cpt kind_t, util::ratio_cpt delta_t>
+using swap_delta = kind<
+    typename kind_t::dimension,
+    typename kind_t::prefix_t,
+    typename kind_t::ratio,
+    delta_t>;
+
 } // namespace kind
+
+namespace magnitude
+{
+
+struct magnitude_tag
+{
+};
+
+template <typename type>
+using is_magnitude = std::is_base_of<magnitude_tag, type>;
+
+template <typename type>
+constexpr bool is_magnitude_v = is_magnitude<type>::value;
+
+template <typename type>
+concept magnitude_cpt = is_magnitude_v<type>;
+
+template <kind::kind_cpt kind_t, typename internal_data_type> struct magnitude
+{
+public:
+    explicit constexpr magnitude(internal_data_type measurement)
+    : _measurement { measurement }
+    {
+    }
+
+    template <kind::kind_cpt new_kind_t>
+    [[nodiscard]]
+    constexpr auto
+    convert_to() -> magnitude<new_kind_t, internal_data_type>
+    {
+        return magnitude<new_kind_t, internal_data_type> {
+            kind::conversion<kind_t, new_kind_t>::apply_to(_measurement)
+        };
+    }
+
+    [[nodiscard]]
+    constexpr auto
+    get_measurement() const noexcept -> internal_data_type
+    {
+        return _measurement;
+    }
+
+private:
+    internal_data_type _measurement;
+};
+
+template <magnitude_cpt magnitude_t, typename internal_data_type>
+[[nodiscard]]
+constexpr auto
+swap_internal_data_type(magnitude_t mag
+) noexcept -> magnitude<typename magnitude_t::kind, internal_data_type>
+{
+    return magnitude<typename magnitude_t::kind, internal_data_type> {
+        mag.get_measurement()
+    };
+}
+
+} // namespace magnitude
 
 } // namespace impl
 
