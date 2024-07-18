a82e582cba3825566bf2493efd185c0959407f04
Ayimany <mooncatayimany@proton.me>
2024-07-18 16:41:32 -0600

    Add Frequency Units

--- include/frequency.hh
+++ include/frequency.hh
@@ -1,28 +1,25 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/time>
+#include <wither/units/time.hh>
 
-namespace lmc::units::frequency
+namespace units
 {
-using hertz = impl::cnt::unit_container<
-    impl::def::definition_reciprocate<time::seconds::definition>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(hertz)
 
-using dimension = hertz::definition::dimension;
-
-template <impl::cnt::cpt::unit_container container>
-using is_frequency_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_frequency_unit_v = is_frequency_unit<container>::value;
-
-} // namespace lmc::units::frequency
-  //
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(frequency, hertz)
-} // namespace ilmc
+
+using hertz = impl::kind_reciprocal<seconds>;
+
+} // namespace kind
+
+using hertz = impl::magnitude<kind::hertz, double>;
+
+namespace dim
+{
+
+using frequency = kind::hertz::dimension;
+
+} // namespace dim
+
+} // namespace units
+
