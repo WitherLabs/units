461be229ea5a56084bb9766a6c7da72005901f3e
Ayimany <mooncatayimany@proton.me>
2024-07-17 18:02:07 -0600

    [MOD] Move temperature over to new format

--- include/temperature.hh
+++ include/temperature.hh
@@ -6,19 +6,25 @@
 
 namespace units
 {
+
+using temperature_dimension = impl::dimension<
+    std::ratio<0>, // length
+    std::ratio<0>, // mass
+    std::ratio<0>, // time
+    std::ratio<0>, // current
+    std::ratio<1>, // temperature
+    std::ratio<0>, // substance
+    std::ratio<0>  // luminous intensity
+    >;
+
 namespace kind
 {
-using kelvin = impl::kind<
-    impl::no_dimension,
-    impl::prefix::none,
-    impl::ratio::base,
-    impl::delta::none>;
-
+using kelvin  = impl::basic_kind<temperature_dimension>;
 using celsius = impl::
     derived_kind<kelvin, impl::ratio::base, std::ratio<27315, 100>>::value;
-
 using fahrenheit
     = impl::derived_kind<celsius, std::ratio<5, 9>, std::ratio<-160, 9>>::value;
+
 } // namespace kind
 
 using kelvin     = impl::magnitude<kind::kelvin, double>;
