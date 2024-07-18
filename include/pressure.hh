9fd04f6cbc2dee296fc3f6053b5810c995058ca5
Ayimany <mooncatayimany@proton.me>
2024-07-18 17:47:23 -0600

    Add New Pressure Units

--- include/pressure.hh
+++ include/pressure.hh
@@ -1,46 +1,78 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/force>
+#include <wither/units/area.hh>
+#include <wither/units/force.hh>
 
-namespace lmc::units::pressure
+namespace units
 {
-using pascals = impl::cnt::unit_container<impl::def::definition_divide<
-    force::newtons::definition,
-    impl::def::definition_squared<length::meters::definition>>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(pascals)
 
-using dimension = pascals::definition::dimension;
-
-template <impl::cnt::cpt::unit_container container>
-using is_pressure_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_pressure_unit_v = is_pressure_unit<container>::value;
-
-using bars = impl::abbr::derive<std::ratio<100000>, pascals>;
-
-using technical_atmospheres
-    = impl::abbr::derive<std::ratio<980665, 10>, pascals>;
-
-using atmospheres = impl::abbr::derive<std::ratio<101325>, pascals>;
-
-using torrs = impl::abbr::derive<std::ratio<1, 760>, atmospheres>;
-
-using pounds_per_square_inch = impl::abbr::derive<
-    std::ratio_add<std::ratio<6894>, std::ratio<47330823, 62500000>>,
-    pascals>;
-
-} // namespace lmc::units::pressure
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(pressure, pascals)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(pressure, bars)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(pressure, technical_atmospheres)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(pressure, atmospheres)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(pressure, torrs)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(pressure, pounds_per_square_inch)
-} // namespace ilmc
+
+// clang-format off
+
+using pascals      = impl::divide_kinds<newtons, metres_squared>;
+using attopascals  = impl::swap_kind_prefix<pascals, impl::prefix::atto>;
+using femtopascals = impl::swap_kind_prefix<pascals, impl::prefix::femto>;
+using picopascals  = impl::swap_kind_prefix<pascals, impl::prefix::pico>;
+using nanopascals  = impl::swap_kind_prefix<pascals, impl::prefix::nano>;
+using micropascals = impl::swap_kind_prefix<pascals, impl::prefix::micro>;
+using millipascals = impl::swap_kind_prefix<pascals, impl::prefix::milli>;
+using centipascals = impl::swap_kind_prefix<pascals, impl::prefix::centi>;
+using decipascals  = impl::swap_kind_prefix<pascals, impl::prefix::deci>;
+using nonepascals  = impl::swap_kind_prefix<pascals, impl::prefix::none>;
+using decapascals  = impl::swap_kind_prefix<pascals, impl::prefix::deca>;
+using hectopascals = impl::swap_kind_prefix<pascals, impl::prefix::hecto>;
+using kilopascals  = impl::swap_kind_prefix<pascals, impl::prefix::kilo>;
+using megapascals  = impl::swap_kind_prefix<pascals, impl::prefix::mega>;
+using gigapascals  = impl::swap_kind_prefix<pascals, impl::prefix::giga>;
+using terapascals  = impl::swap_kind_prefix<pascals, impl::prefix::tera>;
+using petapascals  = impl::swap_kind_prefix<pascals, impl::prefix::peta>;
+using exapascals   = impl::swap_kind_prefix<pascals, impl::prefix::exa>;
+
+using bars                   = impl::swap_kind_prefix<pascals, std::ratio<100000000>>;
+using baryes                 = impl::divide_kinds<dynes, centimetres_squared>;
+using atmospheres            = impl::derived_kind<pascals, std::ratio<101325>>;
+using torrs                  = impl::derived_kind<atmospheres, std::ratio<1, 760>>;
+using pounds_per_square_inch = impl::divide_kinds<pounds_of_force, inches_squared>;
+
+// clang-format on
+
+} // namespace kind
+
+// clang-format off
+
+using pascals      = impl::magnitude<kind::pascals, double>;
+using attopascals  = impl::magnitude<kind::attopascals, double>;
+using femtopascals = impl::magnitude<kind::femtopascals, double>;
+using picopascals  = impl::magnitude<kind::picopascals, double>;
+using nanopascals  = impl::magnitude<kind::nanopascals, double>;
+using micropascals = impl::magnitude<kind::micropascals, double>;
+using millipascals = impl::magnitude<kind::millipascals, double>;
+using centipascals = impl::magnitude<kind::centipascals, double>;
+using decipascals  = impl::magnitude<kind::decipascals, double>;
+using nonepascals  = impl::magnitude<kind::nonepascals, double>;
+using decapascals  = impl::magnitude<kind::decapascals, double>;
+using hectopascals = impl::magnitude<kind::hectopascals, double>;
+using kilopascals  = impl::magnitude<kind::kilopascals, double>;
+using megapascals  = impl::magnitude<kind::megapascals, double>;
+using gigapascals  = impl::magnitude<kind::gigapascals, double>;
+using terapascals  = impl::magnitude<kind::terapascals, double>;
+using petapascals  = impl::magnitude<kind::petapascals, double>;
+using exapascals   = impl::magnitude<kind::exapascals, double>;
+
+using bars                   = impl::magnitude<kind::bars, double>;
+using baryes                 = impl::magnitude<kind::baryes, double>;
+using atmospheres            = impl::magnitude<kind::atmospheres, double>;
+using torrs                  = impl::magnitude<kind::torrs, double>;
+using pounds_per_square_inch = impl::magnitude<kind::pounds_per_square_inch, double>;
+
+// clang-format on
+
+namespace dim
+{
+
+} // namespace dim
+
+} // namespace units
+
