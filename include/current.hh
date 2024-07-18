76f5042fc9ed178b4f3c07bd47920c0c8dce96a5
Ayimany <mooncatayimany@proton.me>
2024-07-17 18:00:38 -0600

    [MOD] Move current to new format

--- include/current.hh
+++ include/current.hh
@@ -1,40 +1,28 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/impl/prefixes>
-#include <mooncat/units/impl/unit_container>
+#include <wither/units/units.hh>
 
-namespace lmc::units::current
+namespace units
 {
 
-using dimension = impl::dim::dimensional_vector<
-    impl::dim::length<0>,
-    impl::dim::mass<0>,
-    impl::dim::time<0>,
-    impl::dim::current<1>,
-    impl::dim::temperature<0>,
-    impl::dim::luminosity<0>,
-    impl::dim::substance<0>>;
+using current_dimension = impl::dimension<
+    std::ratio<0>, // length
+    std::ratio<0>, // mass
+    std::ratio<0>, // time
+    std::ratio<1>, // current
+    std::ratio<0>, // temperature
+    std::ratio<0>, // substance
+    std::ratio<0>  // luminous intensity
+    >;
 
-template <impl::cnt::cpt::unit_container container>
-using is_current_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_current_unit_v = is_current_unit<container>::value;
-
-using amperes = impl::cnt::unit_container<impl::def::unit_definition<
-    dimension,
-    impl::cmp::base_unit_prefix,
-    impl::cmp::base_unit_ratio,
-    impl::cmp::base_unit_delta>>;
-
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(amperes)
-
-} // namespace lmc::units::current
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(current, amperes)
-} // namespace ilmc
+
+using amperes = impl::basic_kind<current_dimension>;
+
+} // namespace kind
+
+using amperes = impl::magnitude<kind::amperes, double>;
+
+} // namespace units
+
