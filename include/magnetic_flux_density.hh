ebc8a6405e2ab03ca71e001b5a24e3b9bfba178b
Ayimany <mooncatayimany@proton.me>
2024-07-18 20:02:42 -0600

    Magnetic Flux Density Units

--- include/magnetic_flux_density.hh
+++ include/magnetic_flux_density.hh
@@ -1,29 +1,66 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/magnetic_flux>
+#include <wither/units/area.hh>
+#include <wither/units/magnetic_flux.hh>
 
-namespace lmc::units::magnetic_flux_density
+namespace units
 {
-using teslas = impl::cnt::unit_container<impl::def::definition_divide<
-    magnetic_flux::webers::definition,
-    impl::def::definition_squared<length::meters::definition>>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(teslas)
 
-using dimension = teslas::definition::dimension;
-
-template <impl::cnt::cpt::unit_container container>
-using is_magnetic_flux_density_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_magnetic_flux_density_unit_v
-    = is_magnetic_flux_density_unit<container>::value;
-
-} // namespace lmc::units::magnetic_flux_density
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(magnetic_flux_density, teslas)
-} // namespace ilmc
+
+// clang-format off
+
+using teslas      = impl::divide_kinds<webers, metres_squared>;
+using attoteslas  = impl::swap_kind_prefix<teslas, impl::prefix::atto>;
+using femtoteslas = impl::swap_kind_prefix<teslas, impl::prefix::femto>;
+using picoteslas  = impl::swap_kind_prefix<teslas, impl::prefix::pico>;
+using nanoteslas  = impl::swap_kind_prefix<teslas, impl::prefix::nano>;
+using microteslas = impl::swap_kind_prefix<teslas, impl::prefix::micro>;
+using milliteslas = impl::swap_kind_prefix<teslas, impl::prefix::milli>;
+using centiteslas = impl::swap_kind_prefix<teslas, impl::prefix::centi>;
+using deciteslas  = impl::swap_kind_prefix<teslas, impl::prefix::deci>;
+using noneteslas  = impl::swap_kind_prefix<teslas, impl::prefix::none>;
+using decateslas  = impl::swap_kind_prefix<teslas, impl::prefix::deca>;
+using hectoteslas = impl::swap_kind_prefix<teslas, impl::prefix::hecto>;
+using kiloteslas  = impl::swap_kind_prefix<teslas, impl::prefix::kilo>;
+using megateslas  = impl::swap_kind_prefix<teslas, impl::prefix::mega>;
+using gigateslas  = impl::swap_kind_prefix<teslas, impl::prefix::giga>;
+using terateslas  = impl::swap_kind_prefix<teslas, impl::prefix::tera>;
+using petateslas  = impl::swap_kind_prefix<teslas, impl::prefix::peta>;
+using exateslas   = impl::swap_kind_prefix<teslas, impl::prefix::exa>;
+
+// clang-format on
+
+} // namespace kind
+
+// clang-format off
+
+using teslas      = impl::magnitude<kind::teslas, double>;
+using attoteslas  = impl::magnitude<kind::attoteslas, double>;
+using femtoteslas = impl::magnitude<kind::femtoteslas, double>;
+using picoteslas  = impl::magnitude<kind::picoteslas, double>;
+using nanoteslas  = impl::magnitude<kind::nanoteslas, double>;
+using microteslas = impl::magnitude<kind::microteslas, double>;
+using milliteslas = impl::magnitude<kind::milliteslas, double>;
+using centiteslas = impl::magnitude<kind::centiteslas, double>;
+using deciteslas  = impl::magnitude<kind::deciteslas, double>;
+using noneteslas  = impl::magnitude<kind::noneteslas, double>;
+using decateslas  = impl::magnitude<kind::decateslas, double>;
+using hectoteslas = impl::magnitude<kind::hectoteslas, double>;
+using kiloteslas  = impl::magnitude<kind::kiloteslas, double>;
+using megateslas  = impl::magnitude<kind::megateslas, double>;
+using gigateslas  = impl::magnitude<kind::gigateslas, double>;
+using terateslas  = impl::magnitude<kind::terateslas, double>;
+using petateslas  = impl::magnitude<kind::petateslas, double>;
+using exateslas   = impl::magnitude<kind::exateslas, double>;
+
+// clang-format on
+
+namespace dim
+{
+
+} // namespace dim
+
+} // namespace units
+
