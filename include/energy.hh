6d0f9cca1f855fc54cac0df803427aa78a103e26
Ayimany <mooncatayimany@proton.me>
2024-07-18 20:03:37 -0600

    Energy Units

--- include/energy.hh
+++ include/energy.hh
@@ -1,55 +1,75 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/force>
+#include <wither/units/force.hh>
 
-namespace lmc::units::energy
+namespace units
 {
-using joules = impl::cnt::unit_container<impl::def::definition_multiply<
-    force::newtons::definition,
-    length::meters::definition>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(joules)
 
-using dimension = joules::definition::dimension;
-
-template <impl::cnt::cpt::unit_container container>
-using is_energy_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_energy_unit_v = is_energy_unit<container>::value;
-
-using foot_pound_force = impl::abbr::
-    derive<std::ratio_add<std::ratio<1>, std::ratio<1779, 5000>>, joules>;
-
-using horsepower_hour = impl::abbr::
-    derive<std::ratio_add<std::ratio<2>, std::ratio<1369, 2000>>, megajoules>;
-
-using kilowatt_hour = impl::abbr::derive<std::ratio<36, 10>, megajoules>;
-using us_therms     = impl::abbr::derive<std::ratio<10548, 100>, megajoules>;
-
-using calories_th   = impl::abbr::derive<std::ratio<4184, 1000>, joules>;
-using calories_4    = impl::abbr::derive<std::ratio<4204, 1000>, joules>;
-using calories_15   = impl::abbr::derive<std::ratio<41855, 10000>, joules>;
-using calories_20   = impl::abbr::derive<std::ratio<4182, 1000>, joules>;
-using calories_mean = impl::abbr::derive<std::ratio<4190, 1000>, joules>;
-using calories_it   = impl::abbr::derive<std::ratio<41868, 10000>, joules>;
-
-using electronvolts
-    = impl::abbr::derive<std::ratio<160218, 1000000>, attojoules>;
-
-using ton_of_tnt = impl::abbr::derive<std::ratio<4184>, joules>;
-
-} // namespace lmc::units::energy
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(energy, joules)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(energy, foot_pound_force)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(energy, horsepower_hour)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(energy, kilowatt_hour)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(energy, calories_it)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(energy, electronvolts)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(energy, ton_of_tnt)
-} // namespace ilmc
+
+// clang-format off
+
+using joules      = impl::multiply_kinds<newtons, metres>;
+using attojoules  = impl::swap_kind_prefix<joules, impl::prefix::atto>;
+using femtojoules = impl::swap_kind_prefix<joules, impl::prefix::femto>;
+using picojoules  = impl::swap_kind_prefix<joules, impl::prefix::pico>;
+using nanojoules  = impl::swap_kind_prefix<joules, impl::prefix::nano>;
+using microjoules = impl::swap_kind_prefix<joules, impl::prefix::micro>;
+using millijoules = impl::swap_kind_prefix<joules, impl::prefix::milli>;
+using centijoules = impl::swap_kind_prefix<joules, impl::prefix::centi>;
+using decijoules  = impl::swap_kind_prefix<joules, impl::prefix::deci>;
+using nonejoules  = impl::swap_kind_prefix<joules, impl::prefix::none>;
+using decajoules  = impl::swap_kind_prefix<joules, impl::prefix::deca>;
+using hectojoules = impl::swap_kind_prefix<joules, impl::prefix::hecto>;
+using kilojoules  = impl::swap_kind_prefix<joules, impl::prefix::kilo>;
+using megajoules  = impl::swap_kind_prefix<joules, impl::prefix::mega>;
+using gigajoules  = impl::swap_kind_prefix<joules, impl::prefix::giga>;
+using terajoules  = impl::swap_kind_prefix<joules, impl::prefix::tera>;
+using petajoules  = impl::swap_kind_prefix<joules, impl::prefix::peta>;
+using exajoules   = impl::swap_kind_prefix<joules, impl::prefix::exa>;
+
+using ergs = impl::multiply_kinds<dynes, centimetres>;
+using kilowatt_hour = impl::derived_kind<megajoules, std::ratio<18, 5>>;
+using british_thermal_unit = impl::derived_kind<joules, std::ratio<52753, 50>>;
+using electronvolt = impl::derived_kind<picojoules, std::ratio<16021773, 100000000000>>;
+
+// clang-format on
+
+} // namespace kind
+
+// clang-format off
+
+using joules      = impl::magnitude<kind::joules, double>;
+using attojoules  = impl::magnitude<kind::attojoules, double>;
+using femtojoules = impl::magnitude<kind::femtojoules, double>;
+using picojoules  = impl::magnitude<kind::picojoules, double>;
+using nanojoules  = impl::magnitude<kind::nanojoules, double>;
+using microjoules = impl::magnitude<kind::microjoules, double>;
+using millijoules = impl::magnitude<kind::millijoules, double>;
+using centijoules = impl::magnitude<kind::centijoules, double>;
+using decijoules  = impl::magnitude<kind::decijoules, double>;
+using nonejoules  = impl::magnitude<kind::nonejoules, double>;
+using decajoules  = impl::magnitude<kind::decajoules, double>;
+using hectojoules = impl::magnitude<kind::hectojoules, double>;
+using kilojoules  = impl::magnitude<kind::kilojoules, double>;
+using megajoules  = impl::magnitude<kind::megajoules, double>;
+using gigajoules  = impl::magnitude<kind::gigajoules, double>;
+using terajoules  = impl::magnitude<kind::terajoules, double>;
+using petajoules  = impl::magnitude<kind::petajoules, double>;
+using exajoules   = impl::magnitude<kind::exajoules, double>;
+
+using ergs                 = impl::magnitude<kind::ergs, double>;
+using kilowatt_hour        = impl::magnitude<kind::kilowatt_hour, double>;
+using british_thermal_unit = impl::magnitude<kind::british_thermal_unit, double>;
+using electronvolt         = impl::magnitude<kind::electronvolt, double>;
+
+// clang-format on
+
+namespace dim
+{
+
+} // namespace dim
+
+} // namespace units
+
