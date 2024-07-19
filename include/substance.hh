8097847c07ab9a19d3bc0f294a46638bf278cc4d
Ayimany <mooncatayimany@proton.me>
2024-07-18 23:08:03 -0600

    Pretty Substance

--- include/substance.hh
+++ include/substance.hh
@@ -2,6 +2,8 @@
 
 #include <wither/units/units.hh>
 
+// clang-format off
+
 namespace units
 {
 
@@ -23,47 +25,47 @@ using substance = impl::dimension<
 namespace kind
 {
 
-using moles = impl::basic_kind<dim::substance>;
-
-using attomoles  = impl::swap_kind_prefix<moles, impl::prefix::atto>;
-using femtomoles = impl::swap_kind_prefix<moles, impl::prefix::femto>;
-using picomoles  = impl::swap_kind_prefix<moles, impl::prefix::pico>;
-using nanomoles  = impl::swap_kind_prefix<moles, impl::prefix::nano>;
-using micromoles = impl::swap_kind_prefix<moles, impl::prefix::micro>;
-using millimoles = impl::swap_kind_prefix<moles, impl::prefix::milli>;
-using centimoles = impl::swap_kind_prefix<moles, impl::prefix::centi>;
-using decimoles  = impl::swap_kind_prefix<moles, impl::prefix::deci>;
-using nonemoles  = impl::swap_kind_prefix<moles, impl::prefix::none>;
-using decamoles  = impl::swap_kind_prefix<moles, impl::prefix::deca>;
-using hectomoles = impl::swap_kind_prefix<moles, impl::prefix::hecto>;
-using kilomoles  = impl::swap_kind_prefix<moles, impl::prefix::kilo>;
-using megamoles  = impl::swap_kind_prefix<moles, impl::prefix::mega>;
-using gigamoles  = impl::swap_kind_prefix<moles, impl::prefix::giga>;
-using teramoles  = impl::swap_kind_prefix<moles, impl::prefix::tera>;
-using petamoles  = impl::swap_kind_prefix<moles, impl::prefix::peta>;
-using examoles   = impl::swap_kind_prefix<moles, impl::prefix::exa>;
+using moles      = impl::basic_kind<dim::substance>; 
+using attomoles  = impl::swap_kind_prefix<moles,     impl::prefix::atto>;
+using femtomoles = impl::swap_kind_prefix<moles,     impl::prefix::femto>;
+using picomoles  = impl::swap_kind_prefix<moles,     impl::prefix::pico>;
+using nanomoles  = impl::swap_kind_prefix<moles,     impl::prefix::nano>;
+using micromoles = impl::swap_kind_prefix<moles,     impl::prefix::micro>;
+using millimoles = impl::swap_kind_prefix<moles,     impl::prefix::milli>;
+using centimoles = impl::swap_kind_prefix<moles,     impl::prefix::centi>;
+using decimoles  = impl::swap_kind_prefix<moles,     impl::prefix::deci>;
+using nonemoles  = impl::swap_kind_prefix<moles,     impl::prefix::none>;
+using decamoles  = impl::swap_kind_prefix<moles,     impl::prefix::deca>;
+using hectomoles = impl::swap_kind_prefix<moles,     impl::prefix::hecto>;
+using kilomoles  = impl::swap_kind_prefix<moles,     impl::prefix::kilo>;
+using megamoles  = impl::swap_kind_prefix<moles,     impl::prefix::mega>;
+using gigamoles  = impl::swap_kind_prefix<moles,     impl::prefix::giga>;
+using teramoles  = impl::swap_kind_prefix<moles,     impl::prefix::tera>;
+using petamoles  = impl::swap_kind_prefix<moles,     impl::prefix::peta>;
+using examoles   = impl::swap_kind_prefix<moles,     impl::prefix::exa>;
 
 } // namespace kind
 
-using moles = impl::magnitude<kind::moles, double>;
-
-using attomoles  = impl::magnitude<kind::attomoles, double>;
+using moles      = impl::magnitude<kind::moles,      double>;
+using attomoles  = impl::magnitude<kind::attomoles,  double>;
 using femtomoles = impl::magnitude<kind::femtomoles, double>;
-using picomoles  = impl::magnitude<kind::picomoles, double>;
-using nanomoles  = impl::magnitude<kind::nanomoles, double>;
+using picomoles  = impl::magnitude<kind::picomoles,  double>;
+using nanomoles  = impl::magnitude<kind::nanomoles,  double>;
 using micromoles = impl::magnitude<kind::micromoles, double>;
 using millimoles = impl::magnitude<kind::millimoles, double>;
 using centimoles = impl::magnitude<kind::centimoles, double>;
-using decimoles  = impl::magnitude<kind::decimoles, double>;
-using nonemoles  = impl::magnitude<kind::nonemoles, double>;
-using decamoles  = impl::magnitude<kind::decamoles, double>;
+using decimoles  = impl::magnitude<kind::decimoles,  double>;
+using nonemoles  = impl::magnitude<kind::nonemoles,  double>;
+using decamoles  = impl::magnitude<kind::decamoles,  double>;
 using hectomoles = impl::magnitude<kind::hectomoles, double>;
-using kilomoles  = impl::magnitude<kind::kilomoles, double>;
-using megamoles  = impl::magnitude<kind::megamoles, double>;
-using gigamoles  = impl::magnitude<kind::gigamoles, double>;
-using teramoles  = impl::magnitude<kind::teramoles, double>;
-using petamoles  = impl::magnitude<kind::petamoles, double>;
-using examoles   = impl::magnitude<kind::examoles, double>;
+using kilomoles  = impl::magnitude<kind::kilomoles,  double>;
+using megamoles  = impl::magnitude<kind::megamoles,  double>;
+using gigamoles  = impl::magnitude<kind::gigamoles,  double>;
+using teramoles  = impl::magnitude<kind::teramoles,  double>;
+using petamoles  = impl::magnitude<kind::petamoles,  double>;
+using examoles   = impl::magnitude<kind::examoles,   double>;
 
 } // namespace units
+
+// clang-format on
 
