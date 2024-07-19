f0581722a07b49e91aaa99430316a29b3f907d36
Ayimany <mooncatayimany@proton.me>
2024-07-18 20:03:43 -0600

    Dose Units

--- include/dose.hh
+++ include/dose.hh
@@ -1,47 +1,103 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/energy>
+#include <wither/units/energy.hh>
 
-namespace lmc::units::dose
+namespace units
 {
 
-namespace absorbed
+namespace kind
 {
-using grays = impl::cnt::unit_container<impl::def::definition_divide<
-    energy::joules::definition,
-    mass::kilograms::definition>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(grays)
 
-} // namespace absorbed
+// clang-format off
 
-namespace equivalent
+using grays      = impl::divide_kinds<joules, kilograms>;
+using attograys  = impl::swap_kind_prefix<grays, impl::prefix::atto>;
+using femtograys = impl::swap_kind_prefix<grays, impl::prefix::femto>;
+using picograys  = impl::swap_kind_prefix<grays, impl::prefix::pico>;
+using nanograys  = impl::swap_kind_prefix<grays, impl::prefix::nano>;
+using micrograys = impl::swap_kind_prefix<grays, impl::prefix::micro>;
+using milligrays = impl::swap_kind_prefix<grays, impl::prefix::milli>;
+using centigrays = impl::swap_kind_prefix<grays, impl::prefix::centi>;
+using decigrays  = impl::swap_kind_prefix<grays, impl::prefix::deci>;
+using nonegrays  = impl::swap_kind_prefix<grays, impl::prefix::none>;
+using decagrays  = impl::swap_kind_prefix<grays, impl::prefix::deca>;
+using hectograys = impl::swap_kind_prefix<grays, impl::prefix::hecto>;
+using kilograys  = impl::swap_kind_prefix<grays, impl::prefix::kilo>;
+using megagrays  = impl::swap_kind_prefix<grays, impl::prefix::mega>;
+using gigagrays  = impl::swap_kind_prefix<grays, impl::prefix::giga>;
+using teragrays  = impl::swap_kind_prefix<grays, impl::prefix::tera>;
+using petagrays  = impl::swap_kind_prefix<grays, impl::prefix::peta>;
+using exagrays   = impl::swap_kind_prefix<grays, impl::prefix::exa>;
+
+using sieverts      = impl::divide_kinds<joules, kilograms>;
+using attosieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::atto>;
+using femtosieverts = impl::swap_kind_prefix<sieverts, impl::prefix::femto>;
+using picosieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::pico>;
+using nanosieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::nano>;
+using microsieverts = impl::swap_kind_prefix<sieverts, impl::prefix::micro>;
+using millisieverts = impl::swap_kind_prefix<sieverts, impl::prefix::milli>;
+using centisieverts = impl::swap_kind_prefix<sieverts, impl::prefix::centi>;
+using decisieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::deci>;
+using nonesieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::none>;
+using decasieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::deca>;
+using hectosieverts = impl::swap_kind_prefix<sieverts, impl::prefix::hecto>;
+using kilosieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::kilo>;
+using megasieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::mega>;
+using gigasieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::giga>;
+using terasieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::tera>;
+using petasieverts  = impl::swap_kind_prefix<sieverts, impl::prefix::peta>;
+using exasieverts   = impl::swap_kind_prefix<sieverts, impl::prefix::exa>;
+
+// clang-format on
+
+} // namespace kind
+
+// clang-format off
+
+using grays      = impl::magnitude<kind::grays, double>;
+using attograys  = impl::magnitude<kind::attograys, double>;
+using femtograys = impl::magnitude<kind::femtograys, double>;
+using picograys  = impl::magnitude<kind::picograys, double>;
+using nanograys  = impl::magnitude<kind::nanograys, double>;
+using micrograys = impl::magnitude<kind::micrograys, double>;
+using milligrays = impl::magnitude<kind::milligrays, double>;
+using centigrays = impl::magnitude<kind::centigrays, double>;
+using decigrays  = impl::magnitude<kind::decigrays, double>;
+using nonegrays  = impl::magnitude<kind::nonegrays, double>;
+using decagrays  = impl::magnitude<kind::decagrays, double>;
+using hectograys = impl::magnitude<kind::hectograys, double>;
+using kilograys  = impl::magnitude<kind::kilograys, double>;
+using megagrays  = impl::magnitude<kind::megagrays, double>;
+using gigagrays  = impl::magnitude<kind::gigagrays, double>;
+using teragrays  = impl::magnitude<kind::teragrays, double>;
+using petagrays  = impl::magnitude<kind::petagrays, double>;
+using exagrays   = impl::magnitude<kind::exagrays, double>;
+
+using sieverts      = impl::magnitude<kind::sieverts, double>;
+using attosieverts  = impl::magnitude<kind::attosieverts, double>;
+using femtosieverts = impl::magnitude<kind::femtosieverts, double>;
+using picosieverts  = impl::magnitude<kind::picosieverts, double>;
+using nanosieverts  = impl::magnitude<kind::nanosieverts, double>;
+using microsieverts = impl::magnitude<kind::microsieverts, double>;
+using millisieverts = impl::magnitude<kind::millisieverts, double>;
+using centisieverts = impl::magnitude<kind::centisieverts, double>;
+using decisieverts  = impl::magnitude<kind::decisieverts, double>;
+using nonesieverts  = impl::magnitude<kind::nonesieverts, double>;
+using decasieverts  = impl::magnitude<kind::decasieverts, double>;
+using hectosieverts = impl::magnitude<kind::hectosieverts, double>;
+using kilosieverts  = impl::magnitude<kind::kilosieverts, double>;
+using megasieverts  = impl::magnitude<kind::megasieverts, double>;
+using gigasieverts  = impl::magnitude<kind::gigasieverts, double>;
+using terasieverts  = impl::magnitude<kind::terasieverts, double>;
+using petasieverts  = impl::magnitude<kind::petasieverts, double>;
+using exasieverts   = impl::magnitude<kind::exasieverts, double>;
+
+// clang-format on
+
+namespace dim
 {
-using sieverts = impl::cnt::unit_container<impl::def::definition_divide<
-    energy::joules::definition,
-    mass::kilograms::definition>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(sieverts)
 
-} // namespace equivalent
+} // namespace dim
 
-static_assert(absorbed::grays::definition::dimension::equals_v<
-              equivalent::sieverts::definition::dimension>);
+} // namespace units
 
-using dimension = absorbed::grays::definition::dimension;
-
-template <impl::cnt::cpt::unit_container container>
-using is_dose_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_dose_unit_v = is_dose_unit<container>::value;
-
-} // namespace lmc::units::dose
-  //
-
-inline namespace ilmc
-{
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(dose::absorbed, grays)
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(dose::equivalent, sieverts)
-} // namespace ilmc
