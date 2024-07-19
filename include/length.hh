e36f0889e4458a35a9c30e9602a8a1596aaaf43a
Ayimany <mooncatayimany@proton.me>
2024-07-18 22:59:00 -0600

    The `column` command is fucking amazing

--- include/length.hh
+++ include/length.hh
@@ -2,6 +2,8 @@
 
 #include <wither/units/units.hh>
 
+// clang-format off
+
 namespace units
 {
 
@@ -23,87 +25,81 @@ using length = impl::dimension<
 namespace kind
 {
 
-// clang-format off
 
-using metres      = impl::basic_kind<dim::length>;
-using attometres  = impl::swap_kind_prefix<metres, impl::prefix::atto >;
+using metres      = impl::basic_kind<dim::length>; 
+using attometres  = impl::swap_kind_prefix<metres, impl::prefix::atto>;
 using femtometres = impl::swap_kind_prefix<metres, impl::prefix::femto>;
-using picometres  = impl::swap_kind_prefix<metres, impl::prefix::pico >;
-using nanometres  = impl::swap_kind_prefix<metres, impl::prefix::nano >;
+using picometres  = impl::swap_kind_prefix<metres, impl::prefix::pico>;
+using nanometres  = impl::swap_kind_prefix<metres, impl::prefix::nano>;
 using micrometres = impl::swap_kind_prefix<metres, impl::prefix::micro>;
 using millimetres = impl::swap_kind_prefix<metres, impl::prefix::milli>;
 using centimetres = impl::swap_kind_prefix<metres, impl::prefix::centi>;
-using decimetres  = impl::swap_kind_prefix<metres, impl::prefix::deci >;
-using nonemetres  = impl::swap_kind_prefix<metres, impl::prefix::none >;
-using decametres  = impl::swap_kind_prefix<metres, impl::prefix::deca >;
+using decimetres  = impl::swap_kind_prefix<metres, impl::prefix::deci>;
+using nonemetres  = impl::swap_kind_prefix<metres, impl::prefix::none>;
+using decametres  = impl::swap_kind_prefix<metres, impl::prefix::deca>;
 using hectometres = impl::swap_kind_prefix<metres, impl::prefix::hecto>;
-using kilometres  = impl::swap_kind_prefix<metres, impl::prefix::kilo >;
-using megametres  = impl::swap_kind_prefix<metres, impl::prefix::mega >;
-using gigametres  = impl::swap_kind_prefix<metres, impl::prefix::giga >;
-using terametres  = impl::swap_kind_prefix<metres, impl::prefix::tera >;
-using petametres  = impl::swap_kind_prefix<metres, impl::prefix::peta >;
-using exametres   = impl::swap_kind_prefix<metres, impl::prefix::exa  >;  
+using kilometres  = impl::swap_kind_prefix<metres, impl::prefix::kilo>;
+using megametres  = impl::swap_kind_prefix<metres, impl::prefix::mega>;
+using gigametres  = impl::swap_kind_prefix<metres, impl::prefix::giga>;
+using terametres  = impl::swap_kind_prefix<metres, impl::prefix::tera>;
+using petametres  = impl::swap_kind_prefix<metres, impl::prefix::peta>;
+using exametres   = impl::swap_kind_prefix<metres, impl::prefix::exa>;
 
-using feet           = impl::derived_kind<metres     , std::ratio<381, 1250 >>;
-using twips          = impl::derived_kind<feet       , std::ratio<1  , 17280>>;
-using thous          = impl::derived_kind<feet       , std::ratio<1  , 12000>>;
-using barleycorns    = impl::derived_kind<feet       , std::ratio<1  , 36   >>;
-using inches         = impl::derived_kind<barleycorns, std::ratio<3  , 1    >>;
-using hands          = impl::derived_kind<inches     , std::ratio<4  , 1    >>;
-using yards          = impl::derived_kind<feet       , std::ratio<3  , 1    >>;
-using chains         = impl::derived_kind<yards      , std::ratio<22 , 1    >>;
-using furlongs       = impl::derived_kind<chains     , std::ratio<10 , 1    >>;
-using miles          = impl::derived_kind<furlongs   , std::ratio<8  , 1    >>;
-using leagues        = impl::derived_kind<miles      , std::ratio<3  , 1    >>;
-using fathoms        = impl::derived_kind<metres     , std::ratio<463, 250  >>;
-using cables         = impl::derived_kind<fathoms    , std::ratio<100, 1    >>;
-using nautical_miles = impl::derived_kind<cables     , std::ratio<10 , 1    >>;
-using links          = impl::derived_kind<feet       , std::ratio<66 , 100  >>;
-using rods           = impl::derived_kind<links      , std::ratio<25 , 1    >>;
-
-// clang-format on
+using feet           = impl::derived_kind<metres,      std::ratio<381, 1250>>;
+using twips          = impl::derived_kind<feet,        std::ratio<1,   17280>>;
+using thous          = impl::derived_kind<feet,        std::ratio<1,   12000>>;
+using barleycorns    = impl::derived_kind<feet,        std::ratio<1,   36>>;
+using inches         = impl::derived_kind<barleycorns, std::ratio<3,   1>>;
+using hands          = impl::derived_kind<inches,      std::ratio<4,   1>>;
+using yards          = impl::derived_kind<feet,        std::ratio<3,   1>>;
+using chains         = impl::derived_kind<yards,       std::ratio<22,  1>>;
+using furlongs       = impl::derived_kind<chains,      std::ratio<10,  1>>;
+using miles          = impl::derived_kind<furlongs,    std::ratio<8,   1>>;
+using leagues        = impl::derived_kind<miles,       std::ratio<3,   1>>;
+using fathoms        = impl::derived_kind<metres,      std::ratio<463, 250>>;
+using cables         = impl::derived_kind<fathoms,     std::ratio<100, 1>>;
+using nautical_miles = impl::derived_kind<cables,      std::ratio<10,  1>>;
+using links          = impl::derived_kind<feet,        std::ratio<66,  100>>;
+using rods           = impl::derived_kind<links,       std::ratio<25,  1>>;
 
 } // namespace kind
 
-// clang-format off
-
-using metres      = impl::magnitude<kind::metres     , double>;
-using attometres  = impl::magnitude<kind::attometres , double>;
-using femtometres = impl::magnitude<kind::femtometres, double>;
-using picometres  = impl::magnitude<kind::picometres , double>;
-using nanometres  = impl::magnitude<kind::nanometres , double>;
-using micrometres = impl::magnitude<kind::micrometres, double>;
-using millimetres = impl::magnitude<kind::millimetres, double>;
-using centimetres = impl::magnitude<kind::centimetres, double>;
-using decimetres  = impl::magnitude<kind::decimetres , double>;
-using nonemetres  = impl::magnitude<kind::nonemetres , double>;
-using decametres  = impl::magnitude<kind::decametres , double>;
-using hectometres = impl::magnitude<kind::hectometres, double>;
-using kilometres  = impl::magnitude<kind::kilometres , double>;
-using megametres  = impl::magnitude<kind::megametres , double>;
-using gigametres  = impl::magnitude<kind::gigametres , double>;
-using terametres  = impl::magnitude<kind::terametres , double>;
-using petametres  = impl::magnitude<kind::petametres , double>;
-using exametres   = impl::magnitude<kind::exametres  , double>;
-
-using feet           = impl::magnitude<kind::feet          , double>;
-using twips          = impl::magnitude<kind::twips         , double>;
-using thous          = impl::magnitude<kind::thous         , double>;
-using barleycorns    = impl::magnitude<kind::barleycorns   , double>;
-using inches         = impl::magnitude<kind::inches        , double>;
-using hands          = impl::magnitude<kind::hands         , double>;
-using yards          = impl::magnitude<kind::yards         , double>;
-using chains         = impl::magnitude<kind::chains        , double>;
-using furlongs       = impl::magnitude<kind::furlongs      , double>;
-using miles          = impl::magnitude<kind::miles         , double>;
-using leagues        = impl::magnitude<kind::leagues       , double>;
-using fathoms        = impl::magnitude<kind::fathoms       , double>;
-using cables         = impl::magnitude<kind::cables        , double>;
+using metres         = impl::magnitude<kind::metres,         double>;
+using attometres     = impl::magnitude<kind::attometres,     double>;
+using femtometres    = impl::magnitude<kind::femtometres,    double>;
+using picometres     = impl::magnitude<kind::picometres,     double>;
+using nanometres     = impl::magnitude<kind::nanometres,     double>;
+using micrometres    = impl::magnitude<kind::micrometres,    double>;
+using millimetres    = impl::magnitude<kind::millimetres,    double>;
+using centimetres    = impl::magnitude<kind::centimetres,    double>;
+using decimetres     = impl::magnitude<kind::decimetres,     double>;
+using nonemetres     = impl::magnitude<kind::nonemetres,     double>;
+using decametres     = impl::magnitude<kind::decametres,     double>;
+using hectometres    = impl::magnitude<kind::hectometres,    double>;
+using kilometres     = impl::magnitude<kind::kilometres,     double>;
+using megametres     = impl::magnitude<kind::megametres,     double>;
+using gigametres     = impl::magnitude<kind::gigametres,     double>;
+using terametres     = impl::magnitude<kind::terametres,     double>;
+using petametres     = impl::magnitude<kind::petametres,     double>;
+using exametres      = impl::magnitude<kind::exametres,      double>;
+using feet           = impl::magnitude<kind::feet,           double>;
+using twips          = impl::magnitude<kind::twips,          double>;
+using thous          = impl::magnitude<kind::thous,          double>;
+using barleycorns    = impl::magnitude<kind::barleycorns,    double>;
+using inches         = impl::magnitude<kind::inches,         double>;
+using hands          = impl::magnitude<kind::hands,          double>;
+using yards          = impl::magnitude<kind::yards,          double>;
+using chains         = impl::magnitude<kind::chains,         double>;
+using furlongs       = impl::magnitude<kind::furlongs,       double>;
+using miles          = impl::magnitude<kind::miles,          double>;
+using leagues        = impl::magnitude<kind::leagues,        double>;
+using fathoms        = impl::magnitude<kind::fathoms,        double>;
+using cables         = impl::magnitude<kind::cables,         double>;
 using nautical_miles = impl::magnitude<kind::nautical_miles, double>;
-using links          = impl::magnitude<kind::links         , double>;
-using rods           = impl::magnitude<kind::rods          , double>;
+using links          = impl::magnitude<kind::links,          double>;
+using rods           = impl::magnitude<kind::rods,           double>;
 
-// clang-format on
 
 } // namespace units
 
+// clang-format on
