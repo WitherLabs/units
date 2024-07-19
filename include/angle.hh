31fb19b337bbb018548e8660f27bcf38c035a9cd
Ayimany <mooncatayimany@proton.me>
2024-07-18 20:04:24 -0600

    Angle Units

--- include/angle.hh
+++ include/angle.hh
@@ -1,33 +1,31 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/length>
+#include <wither/units/length.hh>
 
-namespace lmc::units::angle
+namespace units
 {
 
-constexpr std::intmax_t pi_num = 353981633974483;
-constexpr std::intmax_t pi_den = 2500000000000000;
-
-using pi_ratio = std::ratio_add<std::ratio<3>, std::ratio<pi_num, pi_den>>;
-
-using radians = impl::cnt::unit_container<impl::def::definition_divide<
-    length::meters::definition,
-    length::meters::definition>>;
-
-using dimension = radians::definition::dimension;
-
-using turns
-    = impl::abbr::derive<std::ratio_multiply<std::ratio<2>, pi_ratio>, radians>;
-
-using degrees = impl::abbr::derive<std::ratio<1, 360>, turns>;
-
-} // namespace lmc::units::angle
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(angle, radians)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(angle, turns)
-LMC_UNITS_CREATE_UNIT_LITERAL_OPERATOR(angle, degrees)
-} // namespace ilmc
+
+// clang-format off
+
+using radians = impl::divide_kinds<metres, metres>;
+
+// clang-format on
+
+} // namespace kind
+
+// clang-format off
+
+using radians = impl::magnitude<kind::radians, double>;
+
+// clang-format on
+
+namespace dim
+{
+
+} // namespace dim
+
+} // namespace units
+
