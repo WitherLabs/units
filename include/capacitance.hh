21d2af4a8e1e2d4ef41f8eb64b238ff9e328d0bd
Ayimany <mooncatayimany@proton.me>
2024-07-18 20:04:17 -0600

    Capacitance Units

--- include/capacitance.hh
+++ include/capacitance.hh
@@ -1,30 +1,65 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/charge>
-#include <mooncat/units/voltage>
+#include <wither/units/voltage.hh>
 
-namespace lmc::units::capacitance
+namespace units
 {
-using farads = impl::cnt::unit_container<impl::def::definition_divide<
-    charge::coulombs::definition,
-    voltage::volts::definition>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(farads)
 
-using dimension = farads::definition::dimension;
-
-template <impl::cnt::cpt::unit_container container>
-using is_capacitance_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_capacitance_unit_v = is_capacitance_unit<container>::value;
-
-} // namespace lmc::units::capacitance
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(capacitance, farads)
-} // namespace ilmc
+
+// clang-format off
+
+using farads      = impl::divide_kinds<coulombs, volts>;
+using attofarads  = impl::swap_kind_prefix<farads, impl::prefix::atto>;
+using femtofarads = impl::swap_kind_prefix<farads, impl::prefix::femto>;
+using picofarads  = impl::swap_kind_prefix<farads, impl::prefix::pico>;
+using nanofarads  = impl::swap_kind_prefix<farads, impl::prefix::nano>;
+using microfarads = impl::swap_kind_prefix<farads, impl::prefix::micro>;
+using millifarads = impl::swap_kind_prefix<farads, impl::prefix::milli>;
+using centifarads = impl::swap_kind_prefix<farads, impl::prefix::centi>;
+using decifarads  = impl::swap_kind_prefix<farads, impl::prefix::deci>;
+using nonefarads  = impl::swap_kind_prefix<farads, impl::prefix::none>;
+using decafarads  = impl::swap_kind_prefix<farads, impl::prefix::deca>;
+using hectofarads = impl::swap_kind_prefix<farads, impl::prefix::hecto>;
+using kilofarads  = impl::swap_kind_prefix<farads, impl::prefix::kilo>;
+using megafarads  = impl::swap_kind_prefix<farads, impl::prefix::mega>;
+using gigafarads  = impl::swap_kind_prefix<farads, impl::prefix::giga>;
+using terafarads  = impl::swap_kind_prefix<farads, impl::prefix::tera>;
+using petafarads  = impl::swap_kind_prefix<farads, impl::prefix::peta>;
+using exafarads   = impl::swap_kind_prefix<farads, impl::prefix::exa>;
+
+// clang-format on
+
+} // namespace kind
+
+// clang-format off
+
+using farads      = impl::magnitude<kind::farads, double>;
+using attofarads  = impl::magnitude<kind::attofarads, double>;
+using femtofarads = impl::magnitude<kind::femtofarads, double>;
+using picofarads  = impl::magnitude<kind::picofarads, double>;
+using nanofarads  = impl::magnitude<kind::nanofarads, double>;
+using microfarads = impl::magnitude<kind::microfarads, double>;
+using millifarads = impl::magnitude<kind::millifarads, double>;
+using centifarads = impl::magnitude<kind::centifarads, double>;
+using decifarads  = impl::magnitude<kind::decifarads, double>;
+using nonefarads  = impl::magnitude<kind::nonefarads, double>;
+using decafarads  = impl::magnitude<kind::decafarads, double>;
+using hectofarads = impl::magnitude<kind::hectofarads, double>;
+using kilofarads  = impl::magnitude<kind::kilofarads, double>;
+using megafarads  = impl::magnitude<kind::megafarads, double>;
+using gigafarads  = impl::magnitude<kind::gigafarads, double>;
+using terafarads  = impl::magnitude<kind::terafarads, double>;
+using petafarads  = impl::magnitude<kind::petafarads, double>;
+using exafarads   = impl::magnitude<kind::exafarads, double>;
+
+// clang-format on
+
+namespace dim
+{
+
+} // namespace dim
+
+} // namespace units
 
