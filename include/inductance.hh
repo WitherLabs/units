753e427f67da7357d318aa5ef90b848abc958ef7
Ayimany <mooncatayimany@proton.me>
2024-07-18 20:03:11 -0600

    Inductance Units (But Correct)

--- include/inductance.hh
+++ include/inductance.hh
@@ -1,29 +1,65 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/magnetic_flux>
+#include <wither/units/magnetic_flux.hh>
 
-namespace lmc::units::inductance
+namespace units
 {
-using henrys = impl::cnt::unit_container<impl::def::definition_divide<
-    magnetic_flux::webers::definition,
-    current::amperes::definition>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(henrys)
 
-using dimension = henrys::definition::dimension;
-
-template <impl::cnt::cpt::unit_container container>
-using is_inductance_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_inductance_unit_v = is_inductance_unit<container>::value;
-
-} // namespace lmc::units::inductance
-  //
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(inductance, henrys)
-} // namespace ilmc
+
+// clang-format off
+
+using henrys      = impl::divide_kinds<webers, amperes>;
+using attohenrys  = impl::swap_kind_prefix<henrys, impl::prefix::atto>;
+using femtohenrys = impl::swap_kind_prefix<henrys, impl::prefix::femto>;
+using picohenrys  = impl::swap_kind_prefix<henrys, impl::prefix::pico>;
+using nanohenrys  = impl::swap_kind_prefix<henrys, impl::prefix::nano>;
+using microhenrys = impl::swap_kind_prefix<henrys, impl::prefix::micro>;
+using millihenrys = impl::swap_kind_prefix<henrys, impl::prefix::milli>;
+using centihenrys = impl::swap_kind_prefix<henrys, impl::prefix::centi>;
+using decihenrys  = impl::swap_kind_prefix<henrys, impl::prefix::deci>;
+using nonehenrys  = impl::swap_kind_prefix<henrys, impl::prefix::none>;
+using decahenrys  = impl::swap_kind_prefix<henrys, impl::prefix::deca>;
+using hectohenrys = impl::swap_kind_prefix<henrys, impl::prefix::hecto>;
+using kilohenrys  = impl::swap_kind_prefix<henrys, impl::prefix::kilo>;
+using megahenrys  = impl::swap_kind_prefix<henrys, impl::prefix::mega>;
+using gigahenrys  = impl::swap_kind_prefix<henrys, impl::prefix::giga>;
+using terahenrys  = impl::swap_kind_prefix<henrys, impl::prefix::tera>;
+using petahenrys  = impl::swap_kind_prefix<henrys, impl::prefix::peta>;
+using exahenrys   = impl::swap_kind_prefix<henrys, impl::prefix::exa>;
+
+// clang-format on
+
+} // namespace kind
+
+// clang-format off
+
+using henrys      = impl::magnitude<kind::henrys, double>;
+using attohenrys  = impl::magnitude<kind::attohenrys, double>;
+using femtohenrys = impl::magnitude<kind::femtohenrys, double>;
+using picohenrys  = impl::magnitude<kind::picohenrys, double>;
+using nanohenrys  = impl::magnitude<kind::nanohenrys, double>;
+using microhenrys = impl::magnitude<kind::microhenrys, double>;
+using millihenrys = impl::magnitude<kind::millihenrys, double>;
+using centihenrys = impl::magnitude<kind::centihenrys, double>;
+using decihenrys  = impl::magnitude<kind::decihenrys, double>;
+using nonehenrys  = impl::magnitude<kind::nonehenrys, double>;
+using decahenrys  = impl::magnitude<kind::decahenrys, double>;
+using hectohenrys = impl::magnitude<kind::hectohenrys, double>;
+using kilohenrys  = impl::magnitude<kind::kilohenrys, double>;
+using megahenrys  = impl::magnitude<kind::megahenrys, double>;
+using gigahenrys  = impl::magnitude<kind::gigahenrys, double>;
+using terahenrys  = impl::magnitude<kind::terahenrys, double>;
+using petahenrys  = impl::magnitude<kind::petahenrys, double>;
+using exahenrys   = impl::magnitude<kind::exahenrys, double>;
+
+// clang-format on
+
+namespace dim
+{
+
+} // namespace dim
+
+} // namespace units
+
