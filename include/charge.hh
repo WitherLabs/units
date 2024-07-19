5739e912a620356c40e3d828b0a713b56f7a74de
Ayimany <mooncatayimany@proton.me>
2024-07-18 20:03:55 -0600

    Charge Units

--- include/charge.hh
+++ include/charge.hh
@@ -1,29 +1,66 @@
-// vim: set ft=cpp:
-
 #pragma once
 
-#include <mooncat/units/current>
-#include <mooncat/units/time>
+#include <wither/units/current.hh>
+#include <wither/units/time.hh>
 
-namespace lmc::units::charge
+namespace units
 {
-using coulombs = impl::cnt::unit_container<impl::def::definition_multiply<
-    current::amperes::definition,
-    time::seconds::definition>>;
-LMC_UNITS_ADD_PREFIXES_TO_CONTAINER(coulombs)
 
-using dimension = coulombs::definition::dimension;
-
-template <impl::cnt::cpt::unit_container container>
-using is_charge_unit
-    = dimension::equals<typename container::definition::dimension>;
-
-template <impl::cnt::cpt::unit_container container>
-constexpr bool is_charge_unit_v = is_charge_unit<container>::value;
-
-} // namespace lmc::units::charge
-
-inline namespace ilmc
+namespace kind
 {
-LMC_UNITS_CREATE_PREFIXED_UNIT_LITERAL_OPERATOR(charge, coulombs)
-} // namespace ilmc
+
+// clang-format off
+
+using coulombs      = impl::multiply_kinds<amperes, seconds>;
+using attocoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::atto>;
+using femtocoulombs = impl::swap_kind_prefix<coulombs, impl::prefix::femto>;
+using picocoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::pico>;
+using nanocoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::nano>;
+using microcoulombs = impl::swap_kind_prefix<coulombs, impl::prefix::micro>;
+using millicoulombs = impl::swap_kind_prefix<coulombs, impl::prefix::milli>;
+using centicoulombs = impl::swap_kind_prefix<coulombs, impl::prefix::centi>;
+using decicoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::deci>;
+using nonecoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::none>;
+using decacoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::deca>;
+using hectocoulombs = impl::swap_kind_prefix<coulombs, impl::prefix::hecto>;
+using kilocoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::kilo>;
+using megacoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::mega>;
+using gigacoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::giga>;
+using teracoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::tera>;
+using petacoulombs  = impl::swap_kind_prefix<coulombs, impl::prefix::peta>;
+using exacoulombs   = impl::swap_kind_prefix<coulombs, impl::prefix::exa>;
+
+// clang-format on
+
+} // namespace kind
+
+// clang-format off
+
+using coulombs      = impl::magnitude<kind::coulombs, double>;
+using attocoulombs  = impl::magnitude<kind::attocoulombs, double>;
+using femtocoulombs = impl::magnitude<kind::femtocoulombs, double>;
+using picocoulombs  = impl::magnitude<kind::picocoulombs, double>;
+using nanocoulombs  = impl::magnitude<kind::nanocoulombs, double>;
+using microcoulombs = impl::magnitude<kind::microcoulombs, double>;
+using millicoulombs = impl::magnitude<kind::millicoulombs, double>;
+using centicoulombs = impl::magnitude<kind::centicoulombs, double>;
+using decicoulombs  = impl::magnitude<kind::decicoulombs, double>;
+using nonecoulombs  = impl::magnitude<kind::nonecoulombs, double>;
+using decacoulombs  = impl::magnitude<kind::decacoulombs, double>;
+using hectocoulombs = impl::magnitude<kind::hectocoulombs, double>;
+using kilocoulombs  = impl::magnitude<kind::kilocoulombs, double>;
+using megacoulombs  = impl::magnitude<kind::megacoulombs, double>;
+using gigacoulombs  = impl::magnitude<kind::gigacoulombs, double>;
+using teracoulombs  = impl::magnitude<kind::teracoulombs, double>;
+using petacoulombs  = impl::magnitude<kind::petacoulombs, double>;
+using exacoulombs   = impl::magnitude<kind::exacoulombs, double>;
+
+// clang-format on
+
+namespace dim
+{
+
+} // namespace dim
+
+} // namespace units
+
