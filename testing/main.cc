d8431c1ea7b43b91644bee3b40e9e1bed21d86a5
Ayimany <mooncatayimany@proton.me>
2024-07-26 13:45:58 -0600

    New Example

--- testing/main.cc
+++ testing/main.cc
@@ -1,26 +1,29 @@
-#include <wither/units/length.hh>
+#include <wither/units/acceleration.hh>
 
 #include <iostream>
 #include <ostream>
 
-namespace
-{
-
-auto
-print_megameters(units::mega<units::metres> meters) -> void
-{
-    std::println(std::cout, "{} megameters", meters.get_measurement());
-}
-
-} // namespace
+// Uniform acceleration of 18.5 m/s to 46.1m/s over a span of 2.47s
 
 auto
 main() -> int
 {
-    // 10 meters
-    units::metres distance { 1000000 };
-    print_megameters(distance);
+    constexpr units::metres_per_second v_i { 18.5L };
+    constexpr units::metres_per_second v_f { 46.1L };
+    constexpr units::seconds           time { 2.47L };
 
+    constexpr units::metres_per_second_squared acceleration
+        = (v_f - v_i) / time;
+
+    constexpr units::metres distance
+        = v_i * time + (0.5 * acceleration * time * time);
+
+    std::println(
+        std::cout,
+        "acceleration = {}ms^2; distance = {}m",
+        acceleration.get_measurement(),
+        distance.get_measurement()
+    );
     return 0;
 }
 
