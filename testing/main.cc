482c06f41f91c63c89c201909be8eb3c674bbcae
Ayimany <mooncatayimany@proton.me>
2024-07-26 16:41:28 -0600

    Fix Example Again

--- testing/main.cc
+++ testing/main.cc
@@ -8,24 +8,15 @@
 auto
 main() -> int
 {
-    constexpr units::metres_per_second v_i { 18.5L };
-    constexpr units::metres_per_second v_f { 46.1L };
-    constexpr units::seconds           time { 2.47L };
+    constexpr units::metres_per_second v_i { 18.5 };
+    constexpr units::metres_per_second v_f { 46.1 };
+    constexpr units::seconds           time { 2.47 };
 
     constexpr units::metres_per_second_squared acceleration
         = (v_f - v_i) / time;
 
     constexpr units::metres distance
         = v_i * time + (0.5 * acceleration * time * time);
-
-    constexpr units::metres xyz { 0.01 };
-
-    constexpr bool v1_ = xyz == distance;
-    constexpr bool v2_ = xyz != distance;
-    constexpr bool v3_ = xyz > distance;
-    constexpr bool v4_ = xyz < distance;
-    constexpr bool v5_ = xyz >= distance;
-    constexpr bool v6_ = xyz <= distance;
 
     std::println(
         std::cout,
